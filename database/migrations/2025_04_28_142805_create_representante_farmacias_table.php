5c549c51a45936d0d2f4a726106a61be42d8ebd7
marcas2 <mariacasanova2346@gmail.com>
2025-04-28 09:28:44 -0500

    Creación modelos

---
+++ database/migrations/2025_04_28_142805_create_representante_farmacias_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('representante_farmacias', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('representante_farmacias');
+    }
+};
